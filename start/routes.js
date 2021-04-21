caaac5f02587aa8fef708a7dc3ff65788c999a66
Angel Zapata <angelzapata582@gmail.com>
2021-04-20 23:53:18 -0500

    Revert "Delete routes.js"
    This reverts commit acd3e26c01409b65d0940d161c3732697a41f093.

---
+++ start/routes.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+//Ruta para loguear Usuario
+Route.post('/Login','AuthController.Login')
+//Route.post('/Registro','UserController.store')
+//Ruta para guardar user
+//Metodo resource genera todas los verbos sin tener que especificar si es UPDATE, DELETE, GET O POST
+Route.resource('/Registro','UserController')
+          .apiOnly()
+          .validator(new Map([
+            [['users.store'],['RegistroUser']]
+          ]))
+
+Route.post('/LlenarP','RequestController.postPeticionLlenarP').middleware(['authToken'])
+Route.get('/LlenarP/Check','RequestController.checkPeticionLlenarP').middleware(['auth'])
+
+Route.post('/Regado/Encendido','RequestController.postPeticionHumedad').middleware(['auth'])
+Route.post('/Regado/Apagado','RequestController.cancelarPeticionHumedad').middleware(['auth'])
