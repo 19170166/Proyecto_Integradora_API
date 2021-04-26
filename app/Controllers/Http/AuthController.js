8746f0773214cc13c89f56967ec8e58672ccaea9
Angel Zapata <angelzapata582@gmail.com>
2021-04-26 13:25:30 -0500

    modificacion del response del login

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -9,10 +9,16 @@ class AuthController {
        //Generar token del Usuario
        const token = await auth.attempt(email, password)
        const user = await User.findBy('email',email)
-       response.json({
-           nombre: user.Nombre,
-           token: token.token
-        })
+       if(token && user){
+         response.json({
+            nombre: user.Nombre,
+            token: token.token
+         })
+       }else{
+         response.json({
+            error:'password o email incorrecto...'
+         })
+       }
    }
 }
 module.exports = AuthController
